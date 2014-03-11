3daa9581196ae15593adeb4e10da30136a39716e
Dave Horton <daveh@beachdognet.com>
2014-03-10 22:15:51 -0400

    parsing fixes

--- src/sip-dialog-controller.cpp
+++ src/sip-dialog-controller.cpp
@@ -272,11 +272,11 @@ namespace drachtio {
         boost::shared_ptr<JsonMsg> pMsg = pData->getMsg() ;
         ostringstream o ;
 
-        const char *from, *to, *request_uri, *method, *body, *content_type ;
+        const char *from = NULL, *to = NULL, *request_uri = NULL, *method = NULL, *body = NULL, *content_type = NULL ;
 
         try {
 
-            if( 0 > json_unpack(pMsg->value(), "{s:{s:s,s:s,s:{s:s,s:{s:s,s:s,s:s}}}","data","method",&method,"request_uri",&request_uri,
+            if( 0 > json_unpack(pMsg->value(), "{s:{s:s,s:s,s:{s?s,s:{s?s,s?s,s?s}}}","data","method",&method,"request_uri",&request_uri,
                 "msg","body",&body,"headers","content-type",&content_type,"to",&to,"from",&from) ) {
 
                 DR_LOG(log_error) << "SipDialogController::doSendRequestOutsideDialog - failed parsing message" << endl ;
