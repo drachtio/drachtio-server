06fd3448956a4b149d03c1a857469dceadb61aa4
Dave Horton <daveh@beachdognet.com>
2015-03-20 14:26:12 -0400

    fix race condition causing api responses to get discarded

--- src/client-controller.cpp
+++ src/client-controller.cpp
@@ -288,36 +288,36 @@ namespace drachtio {
     bool ClientController::sendRequestInsideDialog( client_ptr client, const string& clientMsgId, const string& dialogId, const string& startLine, 
         const string& headers, const string& body, string& transactionId ) {
 
+        addApiRequest( client, clientMsgId )  ;
         bool rc = m_pController->getDialogController()->sendRequestInsideDialog( clientMsgId, dialogId, startLine, headers, body, transactionId) ;
         if( 0 != startLine.find("ACK") && rc ) {
             addAppTransaction( client, transactionId ) ;
         }
-        addApiRequest( client, clientMsgId )  ;
 
         return rc ;
     }
     bool ClientController::sendRequestOutsideDialog( client_ptr client, const string& clientMsgId, const string& startLine, const string& headers, 
             const string& body, string& transactionId, string& dialogId ) {
         
+        addApiRequest( client, clientMsgId )  ;
         bool rc = m_pController->getDialogController()->sendRequestOutsideDialog( clientMsgId, startLine, headers, body, transactionId, dialogId) ;
         if( 0 != startLine.find("ACK") && rc ) {
             addAppTransaction( client, transactionId ) ;
         }
-        addApiRequest( client, clientMsgId )  ;
         return rc ;        
     }
     bool ClientController::respondToSipRequest( client_ptr client, const string& clientMsgId, const string& transactionId, const string& startLine, const string& headers, 
         const string& body ) {
 
-        bool rc = m_pController->getDialogController()->respondToSipRequest( clientMsgId, transactionId, startLine, headers, body ) ;
         addApiRequest( client, clientMsgId )  ;
+        bool rc = m_pController->getDialogController()->respondToSipRequest( clientMsgId, transactionId, startLine, headers, body ) ;
         return rc ;               
     }   
     bool ClientController::sendCancelRequest( client_ptr client, const string& clientMsgId, const string& transactionId, const string& startLine, const string& headers, 
         const string& body ) {
 
-        bool rc = m_pController->getDialogController()->sendCancelRequest( clientMsgId, transactionId, startLine, headers, body ) ;
         addApiRequest( client, clientMsgId )  ;
+        bool rc = m_pController->getDialogController()->sendCancelRequest( clientMsgId, transactionId, startLine, headers, body ) ;
         return rc ;               
     }
     bool ClientController::proxyRequest( client_ptr client, const string& clientMsgId, const string& transactionId, 
