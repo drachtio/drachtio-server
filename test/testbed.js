0a409362f743e53148edfb24da58349d41eb6208
Dave Horton <daveh@beachdognet.com>
2018-06-09 23:08:49 -0400

    travis fixes

---
+++ test/testbed.js
@@ -0,0 +1,65 @@
+const { spawn } = require('child_process');
+const assert = require('assert');
+const debug = require('debug')('drachtio:server-test');
+const obj = module.exports = {} ;
+
+let drachtio = null;
+let router = null;
+
+obj.start = (confPath) => {
+  confPath = confPath || './drachtio.conf.xml';
+  debug(`starting drachtio with config file: ${confPath}`);
+  assert(!drachtio);
+
+  return new Promise((resolve, reject) => {
+    drachtio = spawn('../build/drachtio', ['-f', confPath], {
+      detached: true,
+      stdio: ['ignore', 'pipe', 'pipe']
+    });
+    drachtio.on('error', (err) => {
+      console.log(`Failed to start subprocess: ${err}`);
+      reject(err);
+    });
+
+    drachtio.stdout.on('data', (data) => {
+      debug(`${data.toString()}`);
+    });
+    drachtio.stderr.on('data', (data) => {
+      debug(`${data.toString()}`);
+    });
+
+    router = spawn('node', ['./scripts/call-router'], {
+      detached: true,
+      stdio: ['ignore', 'pipe', 'pipe']
+    });
+    router.on('error', (err) => {
+      console.log(`failed to start call router: ${err}`);
+      reject(err);
+    });
+    router.stdout.on('data', (data) => {
+      debug(`${data.toString()}`);
+    });
+    router.stderr.on('data', (data) => {
+      console.log(`${data.toString()}`);
+    });
+
+    setTimeout(() => {
+      resolve(drachtio);
+    }, 200);
+  });
+};
+
+obj.stop = () => {
+  assert(drachtio);
+  assert(router);
+
+  return new Promise((resolve, reject) => {
+    drachtio.kill();
+    router.kill();
+    drachtio = null;
+    router = null;
+    setTimeout(() => {
+      resolve();
+    }, 200);
+  });
+};
