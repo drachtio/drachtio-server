drachtio/drachtio-server

commit 2fa3628cfccb81b1a84e21441ad36c2e52820ee0

added some nat support: establishing dialogs to sip clients behind a nat (i.e. uac scenario), also fix bug experienced in EC2 deployment where Contact header of uac INVITE used local address rather than public address

src/sip-dialog-controller.cpp
@@ -25,6 +25,8 @@ THE SOFTWARE.
 #include <boost/algorithm/string/find.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
+#include <sofia-sip/su_alloc.h>
+
 namespace drachtio {
     class SipDialogController ;
 }
@@ -125,6 +127,7 @@ namespace drachtio {
         string myHostport ;
         string requestUri ;
         string name ;
+        string routeUri;
 
         DR_LOG(log_debug) << "SipDialogController::doSendRequestInsideDialog dialog id: " << pData->getDialogId()  ;
 
@@ -147,15 +150,14 @@ namespace drachtio {
                 throw std::runtime_error("unable to find dialog for dialog id provided") ;
             }
 
-            string routeUrl ;
             if (dlg->getRole() == SipDialog::we_are_uas) {
                 // removed this...the sofia uas leg has already had the record-route/contact dealt with to determine route
                 /*
                 string sourceAddress = dlg->getSourceAddress() ;
                 unsigned int sourcePort = dlg->getSourcePort() ;
-                routeUrl = string("sip:") + sourceAddress + ":" + boost::lexical_cast<std::string>(sourcePort) + 
+                routeUri = string("sip:") + sourceAddress + ":" + boost::lexical_cast<std::string>(sourcePort) + 
                     ";transport=" + dlg->getProtocol() ;
-                DR_LOG(log_debug) << "SipDialogController::doSendRequestInsideDialog - sending request to " << routeUrl ;  
+                DR_LOG(log_debug) << "SipDialogController::doSendRequestInsideDialog - sending request to " << routeUri ;  
                 */              
             }
             string transport ;
@@ -164,6 +166,19 @@ namespace drachtio {
 
             tport_t* tp = dlg->getTport() ; 
             bool forceTport = NULL != tp ;  
+
+            //if user supplied all or part of the Contact in a REFER request use it
+            string contact ;
+            if( sip_method_refer == method && forceTport && searchForHeader( tags, siptag_contact_str, contact ) ) {
+                if( string::npos != contact.find("localhost") ) {
+            	    const tp_name_t* tpn = tport_name( tport_parent( tp ) );
+                    string host = tpn->tpn_host ;
+                    string port = tpn->tpn_port ;
+                    if( !replaceHostInUri( contact, host.c_str(), port.c_str() ) ) {
+                        throw std::runtime_error(string("invalid contact value provided by client: ") + contact ) ;
+                    }                    
+                }
+            } 
 
             //nta_leg_t *leg = nta_leg_by_call_id( m_pController->getAgent(), dlg->getCallId().c_str() );
             nta_leg_t *leg = dlg->getNtaLeg();
@@ -201,10 +216,22 @@ namespace drachtio {
                 dlg->setLocalSdp( body.c_str() ) ;
             }
 
+            if (dlg->getRouteUri(routeUri)) {
+                su_home_t* home = theOneAndOnlyController->getHome();
+                url_t *url = url_make(home, requestUri.c_str());
+                if (url) {
+                    if (isRfc1918(url->url_host)) {
+                        DR_LOG(log_debug) << "SipDialogController::doSendRequestInsideDialog - sending request to natt'ed address using route " << routeUri ;
+                    }
+                    else routeUri.clear();
+                    su_free(home, (void *) url);
+                }
+            }
+
             if( sip_method_ack == method ) {
                 if( 200 == dlg->getSipStatus() ) {
                     orq = nta_outgoing_tcreate(leg, NULL, NULL, 
-                        NULL,
+                        routeUri.empty() ? NULL : URL_STRING_MAKE(routeUri.c_str()),                     
                         method, name.c_str(),
                         URL_STRING_MAKE(requestUri.c_str()),
                         TAG_IF( body.length(), SIPTAG_PAYLOAD_STR(body.c_str())),
@@ -224,7 +251,7 @@ namespace drachtio {
                 }
                 orq = nta_outgoing_prack(leg, iip->orq(), response_to_request_inside_dialog, (nta_outgoing_magic_t*) m_pController, 
                     //NULL, 
-                    URL_STRING_MAKE( routeUrl.c_str() ),
+                    routeUri.empty() ? NULL : URL_STRING_MAKE(routeUri.c_str()),
                     NULL, TAG_NEXT(tags) ) ;
             }
             else {
@@ -244,7 +271,7 @@ namespace drachtio {
 
                 }
                 orq = nta_outgoing_tcreate( leg, response_to_request_inside_dialog, (nta_outgoing_magic_t*) m_pController, 
-                    routeUrl.empty() ? NULL : URL_STRING_MAKE(routeUrl.c_str()),                     
+                    routeUri.empty() ? NULL : URL_STRING_MAKE(routeUri.c_str()),                     
                     method, name.c_str()
                     ,URL_STRING_MAKE(requestUri.c_str())
                     ,TAG_IF( addContact, SIPTAG_CONTACT_STR( contact.c_str() ) )
@@ -443,11 +470,13 @@ namespace drachtio {
 
                 tp = (tport_t *) pSelectedTransport->getTport() ;
                 DR_LOG(log_debug) << "SipProxyController::doSendRequestOutsideDialog selected transport " << std::hex << (void*)tp ;
+                DR_LOG(log_debug) << "SipProxyController::doSendRequestOutsideDialog selected transport " << desc ;
                 forceTport = true ;
             }
             su_free( m_pController->getHome(), sip_request ) ;
 
-            tagi_t* tags = makeTags( pData->getHeaders(), desc ) ;
+            tagi_t* tags = makeTags( pData->getHeaders(), desc, 
+                pSelectedTransport->hasExternalIp() ? pSelectedTransport->getExternalIp().c_str() : NULL ) ;
            
             //if user supplied all or part of the From use it
             string from, to, callid ;

src/drachtio.cpp
@@ -633,11 +633,16 @@ namespace drachtio {
         delete [] tags ; 
     }
 
-    tagi_t* makeTags( const string&  hdrs, const string& transport ) {
+    tagi_t* makeTags( const string&  hdrs, const string& transport, const char* szExternalIP ) {
         vector<string> vec ;
         string proto, host, port, myHostport ;
         
         parseTransportDescription(transport, proto, host, port ) ;
+
+        if (szExternalIP) {
+            host = szExternalIP;
+            DR_LOG(log_debug) << "makeTags - using external IP as replacement for 'localhost': " << szExternalIP  ;
+        }
 
         splitLines( hdrs, vec ) ;
         int nHdrs = vec.size() ;
@@ -693,6 +698,7 @@ namespace drachtio {
                     0 == hdr.compare("to") ||
                     0 == hdr.compare("p_asserted_identity") ) ) {
 
+                    DR_LOG(log_debug) << "makeTags - hdr '" << hdrName << "' replacing host with " << host;
                     replaceHostInUri( hdrValue, host.c_str(), port.c_str() ) ;
                 }
                 int len = hdrValue.length() ;
@@ -722,7 +728,28 @@ namespace drachtio {
 
         return tags ;   //NB: caller responsible to delete after use to free memory      
     }
- 
+ 	bool isRfc1918(const char* szHost) {
+        string str = szHost;
+        boost::char_separator<char> sep(".") ;
+        tokenizer tok(str, sep) ;
+        vector<int> vec;
+        for (tokenizer::iterator it = tok.begin(); it != tok.end(); it++) {
+            vec.push_back(boost::lexical_cast<int>(*it));
+        }
+        if (vec.size() == 4) {
+
+            // class A
+            if (vec[0] == 10) return true;
+
+            // class B
+            if (vec[0] == 172 && (vec[1] > 15 || vec[1] < 32)) return true;
+
+            // class C
+            if (vec[0] == 192 && vec[1] == 168) return true;
+        }
+        return false;
+     }
+
     string urlencode(const string &s) {
         static const char lookup[]= "0123456789abcdef";
         std::stringstream e;

src/drachtio.h
@@ -141,7 +141,7 @@ namespace drachtio {
 
 	bool GetValueForHeader( const string& headers, const char *szHeaderName, string& headerValue ) ;
 
-	tagi_t* makeTags( const string& hdrs, const string& transport ) ;
+	tagi_t* makeTags( const string& hdrs, const string& transport, const char* szExternalIP = NULL ) ;
 	void deleteTags( tagi_t* tags ) ;
 
 	int ackResponse( msg_t* msg ) ;
@@ -152,6 +152,8 @@ namespace drachtio {
 	string urlencode(const string &s);
 
   int utf8_strlen(const string& str);
+
+	bool isRfc1918(const char* szHost);
 
 	static char const rfc3261prefix[] =  "z9hG4bK" ;
 

commit 940c206fa368eb6be19d18c85499c8261c3e9894

fix for #51, a crashing bug in non-udp scenarios introduced in 2fa3628cfccb81b1a84e21441ad36c2e52820ee0

src/sip-dialog-controller.cpp
@@ -475,8 +475,13 @@ namespace drachtio {
             }
             su_free( m_pController->getHome(), sip_request ) ;
 
-            tagi_t* tags = makeTags( pData->getHeaders(), desc, 
-                pSelectedTransport->hasExternalIp() ? pSelectedTransport->getExternalIp().c_str() : NULL ) ;
+            tagi_t* tags; 
+            if (pSelectedTransport && pSelectedTransport->hasExternalIp()) {
+                tags = makeTags( pData->getHeaders(), desc, pSelectedTransport->getExternalIp().c_str()) ;
+            }
+            else {
+                tags = makeTags( pData->getHeaders(), desc, NULL) ;
+            }
            
             //if user supplied all or part of the From use it
             string from, to, callid ;