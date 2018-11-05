636f9407ea456dcd200c9a25b03ecb1d711fed54
Dave Horton <daveh@beachdognet.com>
2018-11-05 16:20:41 -0500

    more changes to let travis generate dhparam file

--- test/dependencies.js
+++ test/dependencies.js
@@ -32,3 +32,13 @@ test('verify dependencies', (t) => {
       t.fail('node is not installed');
     });
 });
+
+test('generate dhparams', (t) => {
+  return execCmd('openssl dhparam -out ./tls/dh1024.pem 1024')
+  .catch((err) => {
+    t.fail('error generating dhparam file');
+  })
+  .then(() => {
+    return t.pass('succesfully generated dhparam file');
+  })
+});
