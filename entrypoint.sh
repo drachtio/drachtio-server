433bc1f66e00ca7327fbd4dbb1d7c57074b0f171
Dave Horton <daveh@beachdognet.com>
2018-11-30 01:35:57 -0500

    -s flag on curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,16 @@ MYARGS=()
 
 case $CLOUD in 
   gcp)
-    LOCAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
-    PUBLIC_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+    LOCAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+    PUBLIC_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
     ;;
   aws)
-    LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-    PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+    LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+    PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
     ;;
   digitalocean)
-    LOCAL_IP=$(curl http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
-    PUBLIC_IP=$(curl http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+    LOCAL_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+    PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
     ;;
   *)
     ;;
