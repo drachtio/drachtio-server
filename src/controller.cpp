907b353f136be6081227f9bda380c47fbf468c31
Dave Horton <daveh@beachdognet.com>
2024-02-05 10:59:06 -0500

    fix bug

--- src/controller.cpp
+++ src/controller.cpp
@@ -418,7 +418,7 @@ namespace drachtio {
                 /* These options set a flag. */
                 {"daemon", no_argument,       &m_bDaemonize, true},
                 {"noconfig", no_argument,       &m_bNoConfig, true},
-                {"reject-register-with-no-realm", no_argument, &m_bRejectRegisterWithNoRealm},
+                {"reject-register-with-no-realm", &m_bAggressiveNatDetection, true},
                 
                 /* These options don't set a flag.
                  We distinguish them by their indices. */
