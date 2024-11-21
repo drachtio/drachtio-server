6e77dc7b640c8c08ca87b8132ff81e88ddaf92ab
Dave Horton <daveh@beachdognet.com>
2024-11-21 11:04:34 -0500

    copyright notice

--- src/sip-transports.cpp
+++ src/sip-transports.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013, David C Horton
+Copyright (c) 2024, FirstFive8, Inc
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
